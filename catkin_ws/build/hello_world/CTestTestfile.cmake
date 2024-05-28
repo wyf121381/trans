262db30419a2746320291ebe22ed6ad200205f79
wyf121381 <2297458237@qq.com>
2024-05-28 14:53:07 +0800

    Add a new file

---
+++ catkin_ws/build/hello_world/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/abc/catkin_ws/src/hello_world
+# Build directory: /home/abc/catkin_ws/build/hello_world
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
